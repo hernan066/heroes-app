3ff7a60ad5989a11fa8426edb5fb3854b1a8cd27
hernangit config --global user.name John <hernan0066@gmail.com>
2021-09-16 23:50:30 -0300

    1.14.5

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { LoginScreen } from "../components/login/LoginScreen";
-import { MarvelScreen } from "../components/marvel/MarvelScreen";
-import { Navbar } from "../components/ui/Navbar";
+
+import { DashboardRoutes } from "./DashboardRoutes";
 
 export const AppRouter = () => {
-    return (
-        <Router>
+  return (
+    <Router>
       <div>
-        <Navbar>
-
-
-        </Navbar>
-
-        
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/" component={ MarvelScreen } />
+          <Route path="/" component={DashboardRoutes} />
         </Switch>
       </div>
     </Router>
-    )
-}
+  );
+};
